7a4d5291636f679b6481ea5214563148e1e64dff
priyadarshu <priyadarshinichettiar@gmail.com>
2021-01-04 01:21:10 +0530

    added new image, added gridview code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,31 +26,193 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
+    final double itemWidth = size.width / 2;
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Text(
-                  'Hi Mike',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14.0,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          //color: Colors.white,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Hi, Mike!',
+                      textAlign: TextAlign.left,
+                      style: GoogleFonts.pacifico(
+                        textStyle: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage('assets/images/1.jpg'),
+                        radius: 40.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GridView.count(
+                crossAxisCount: 2,
+                //mainAxisCount:2,
+                shrinkWrap: true,
+                primary: false,
+                padding: EdgeInsets.all(8.0),
+                mainAxisSpacing: 8.0,
+                crossAxisSpacing: 8.0,
+                childAspectRatio: (itemWidth / itemHeight) * 2,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    alignment: Alignment.bottomLeft,
+                    decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Home'),
+                              Text('Events happening'),
+                            ],
+                          ),
+                        ),
+                        Icon(Icons.home),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    alignment: Alignment.bottomLeft,
+                    decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Home'),
+                              Text('Events happening'),
+                            ],
+                          ),
+                        ),
+                        Icon(Icons.home),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    alignment: Alignment.bottomLeft,
+                    decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Home'),
+                              Text('Events happening'),
+                            ],
+                          ),
+                        ),
+                        Icon(Icons.home),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    alignment: Alignment.bottomLeft,
+                    decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Home'),
+                              Text('Events happening'),
+                            ],
+                          ),
+                        ),
+                        Icon(Icons.home),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Events happening',
+                  style: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
                   ),
                 ),
-                Container(
-                  alignment: Alignment.centerRight,
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage('assets\images\1.jpg'),
-                    radius: 40.0,
+              ),
+              /* Row(
+                children: [
+                  GridView.count(
+                    crossAxisCount: 2,
+                    //mainAxisCount:2,
+                    shrinkWrap: true,
+                    primary: false,
+                    padding: EdgeInsets.all(8.0),
+                    mainAxisSpacing: 4.0,
+                    crossAxisSpacing: 4.0,
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      Container(
+                        height: 200.0,
+                        color: Colors.amber,
+                      ),
+                    ],
                   ),
-                ),
-              ],
-            )
-          ],
+                ],
+              ),*/
+            ],
+          ),
         ),
       ),
     );
